70eaa9225acc22d71c13ee47dbd93dde5295401d
pphilip-resume <146376119+pphilip-resume@users.noreply.github.com>
2024-01-19 16:20:22 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    zoom: 6,
+    center: { lat: 51.499995,, lng: -86.700001 }, 
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
